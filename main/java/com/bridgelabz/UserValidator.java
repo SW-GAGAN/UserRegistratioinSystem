707df727caf5c588e4337e783e992e5f39e6ee58
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-03 22:01:26 +0530

    User needs to enter Valid First name

---
+++ main/java/com/bridgelabz/UserValidator.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+    private String firstName;
+
+    public String getFirstName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your first name");
+        firstName = scanner.nextLine();
+        return firstName;
+    }
+
+    public boolean validateName(String name) {
+        Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+");
+        Matcher matcher = pattern.matcher(name);
+        boolean matches = matcher.find();
+        return matches;
+    }
+}
---
+++ test/java/com/bridgelabz/UserValidatorTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class UserValidatorTest {
+    UserValidator userValidator = new UserValidator();
+    @Test
+    public void givenFirst_whenProper_shouldReturnTrue() {
+        boolean result = userValidator.validateName("Gagan");
+        Assert.assertEquals(result,true);
+
+    }
+}
