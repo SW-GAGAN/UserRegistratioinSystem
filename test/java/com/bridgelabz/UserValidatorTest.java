b6689696ca36308f98d6d8b0578b2db5e00d79be
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-03 22:09:12 +0530

    User needs to enter Valid Last Name

--- test/java/com/bridgelabz/UserValidatorTest.java
+++ test/java/com/bridgelabz/UserValidatorTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class UserValidatorTest {
     UserValidator userValidator = new UserValidator();
+
     @Test
     public void givenFirst_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateName("Gagan");
-        Assert.assertEquals(result,true);
+        boolean result = userValidator.validateLastName("Gagan");
+        Assert.assertEquals(result, true);
 
     }
-}
 
+    @Test
+    public void givenLastName_whenProper_shouldReturnTrue() {
+        boolean result = userValidator.validateLastName("Reddy");
+        Assert.assertEquals(result, true);
+    }
+}
