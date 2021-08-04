0700d91cd2c6d97d17aaa990eb7c41dcffb148f5
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-04 22:01:11 +0530

    Password contains at least one numaric number

--- test/java/com/bridgelabz/UserValidatorTest.java
+++ test/java/com/bridgelabz/UserValidatorTest.java
@@ -1,37 +1,38 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class UserValidatorTest {
     UserValidator userValidator = new UserValidator();
 
+
     @Test
     public void givenFirst_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateLastName("Gagan");
-        Assert.assertEquals(result, true);
+        boolean result = userValidator.validateFirstName("Gagan");
+        Assert.assertEquals(true,result);
 
     }
 
     @Test
     public void givenLastName_whenProper_shouldReturnTrue() {
         boolean result = userValidator.validateLastName("Reddy");
-        Assert.assertEquals(result, true);
+        Assert.assertEquals(true,result);
     }
     @Test
     public void givenEmailId_whenProper_shouldReturnTrue() {
         boolean result = userValidator.validateEmail("xyz@gmail.com");
-        Assert.assertEquals(result, true);
+        Assert.assertEquals(true,result);
     }
     @Test
     public void givenMobileNumber_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateMobileNumber("+91 8722289619");
-        Assert.assertEquals(result, true);
+        boolean result = userValidator.validateMobileNumber("+91-8722289617");
+        Assert.assertEquals(true,result);
     }
     @Test
     public void givenPassword_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validatePassword("gaganred");
-        Assert.assertEquals(result, true);
+        boolean result = userValidator.validatePassword("Gaganreddy");
+        Assert.assertEquals(true,result);
     }
 
 }
