98989337c5a6ecc7a695271c9da356679ec8c071
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-06 21:37:46 +0530

    Paramater passed to multiple email

---
+++ test/java/com/bridgelabz/EmailValidationTest.java
@@ -0,0 +1,48 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidationTest {
+    private String email;
+    private boolean expectedResult;
+    private UserValidator registerdEmail;
+
+    public EmailValidationTest(boolean expected, String email) {
+        super();
+        this.email = email;
+        this.expectedResult = expected;
+    }
+
+    @Before
+    public void initialize() {
+        registerdEmail = new UserValidator();
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{
+                {true, "abc@yahoo.com"},
+                {true, "abc-100@yahoo.com"},
+                {true, "abc.100@yahoo.com"},
+                {true, "abc111@abc.com"},
+                {true, "abc-100@abc.net"},
+                {true, "abc.100@abc.com.au"},
+                {true, "abc@1.com"},
+        });
+    }
+
+    @Test
+    public void testEmailValidation() {
+        System.out.println("expected result for email is " + expectedResult);
+        Assertions.assertEquals(expectedResult, registerdEmail.validateEmail(email));
+
+    }
+}
