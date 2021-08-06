0848cb89cd4c9949e6dbcd7943170347c4cced77
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-06 21:31:35 +0530

    Refactor some Changes on mobile number and password

--- test/java/com/bridgelabz/UserValidatorTest.java
+++ test/java/com/bridgelabz/UserValidatorTest.java
@@ -1,38 +1,76 @@
 package com.bridgelabz;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UserValidatorTest {
-    UserValidator userValidator = new UserValidator();
-
+    UserValidator userValidatorTest = new UserValidator();
 
     @Test
-    public void givenFirst_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateFirstName("Gagan");
-        Assert.assertEquals(true,result);
-
+    public void givenFirstName_whenProper_shouldReturnTrue() {
+        boolean result = userValidatorTest.validateName("Gagan");
+        Assertions.assertEquals(true, result);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void givenFirstName_whenProper_shouldReturnFalse() {
+        boolean result = userValidatorTest.validateName("Gag");
+        Assertions.assertEquals(false, result);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void givenFirstName_whenProperShouldReturnFalse() {
+        boolean result = userValidatorTest.validateName("ga");
+        Assertions.assertEquals(false, result);
+    }
+
+    @org.junit.jupiter.api.Test
+
     public void givenLastName_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateLastName("Reddy");
-        Assert.assertEquals(true,result);
+        boolean result = userValidatorTest.validateLName("Reddy");
+        Assertions.assertEquals(true, result);
     }
-    @Test
-    public void givenEmailId_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateEmail("xyz@gmail.com");
-        Assert.assertEquals(true,result);
+
+    @org.junit.jupiter.api.Test
+
+    public void givenLastName_whenProper_shouldReturnFalse() {
+        boolean result = userValidatorTest.validateLName("reddy");
+        Assertions.assertEquals(false, result);
     }
-    @Test
+
+    @org.junit.jupiter.api.Test
+
     public void givenMobileNumber_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateMobileNumber("+91-8722289619");
-        Assert.assertEquals(true,result);
+        boolean result = userValidatorTest.validateNumber("+91 8722289619");
+        Assertions.assertEquals(true, result);
     }
-    @Test
+
+    @org.junit.jupiter.api.Test
+
+    public void givenMobileNumber_whenProper_shouldReturnFalse() {
+        boolean result = userValidatorTest.validateNumber("+91- 28722289619");
+        Assertions.assertEquals(false, result);
+    }
+
+    @org.junit.jupiter.api.Test
+
     public void givenPassword_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validatePassword("Gaganreddy");
-        Assert.assertEquals(true,result);
+        boolean result = userValidatorTest.validatePass("Ga@xyzzss1");
+        Assertions.assertEquals(true, result);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void givenEmailId_ValidShould_ReturnTrue() {
+        boolean result = userValidatorTest.validateEmail("gagangmail.com.");
+        Assertions.assertEquals(true, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void givenEmailId_ValidShould_ReturnFalse() {
+        boolean result = userValidatorTest.validateEmail("gagan-100.@.gmail.com");
+        Assertions.assertEquals(false, result);
     }
 
 }
